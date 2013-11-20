e4eb29699a1a9b397ab7d31eb30242b745ae806e
latitude98 <latitude98@gmail.com>
2013-11-20 12:35:56 -0800

    update lab5 rdoc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
+=begin
+=This is test
+* Comment one
+* Comment two
+1. enumerated list 1
+2. enumberated list 2
+=end
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
 end
